0640d599404baf99e1ed37018adffe11a82b4182
hsc-soft <hsc.wevois@gmail.com>
2021-08-20 15:48:40 +0530

    ward marking survey committed

--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -15,14 +15,72 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FirestoreSettingsToken } from '@angular/fire/firestore';
 
+
+
 let firebase = {
   apiKey: "AIzaSyBGZ_IB4y5Ov1nuqIhWndGU8hfJadlE85I",
-    authDomain: "dtdnavigator.firebaseapp.com",
-    databaseURL: "https://dtdreengus.firebaseio.com",
-    projectId: "dtdnavigator",
-    storageBucket: "dtdnavigator.appspot.com",
-    messagingSenderId: "381118272786",
+  authDomain: "dtdnavigator.firebaseapp.com",
+  databaseURL: "https://dtdnavigator.firebaseio.com",
+  projectId: "dtdnavigator",
+  storageBucket: "dtdnavigator.appspot.com",
+  messagingSenderId: "381118272786",
 };
+
+let databaseName = "dtdnavigator";
+let databaseURL = "https://dtdnavigator.firebaseio.com";
+
+let city = localStorage.getItem("cityName");
+if (city != null) {
+  if (city == 'sikar') {
+    firebase = {
+      apiKey: "AIzaSyA1ZU5hI7Fho0B4ZJO2w8-fsCKMbq95m4c",
+      authDomain: "dtdnavigator.firebaseapp.com",
+      databaseURL: "https://dtdnavigator.firebaseio.com",
+      projectId: "dtdnavigator",
+      storageBucket: "dtdnavigator.appspot.com",
+      messagingSenderId: "381118272786"
+    };
+    databaseName = "dtdnavigator";
+    databaseURL = "https://dtdnavigator.firebaseio.com";
+  }
+  else if (city == 'reengus') {
+    firebase = {
+      apiKey: "AIzaSyBGZ_IB4y5Ov1nuqIhWndGU8hfJadlE85I",
+      authDomain: "dtdnavigator.firebaseapp.com",
+      databaseURL: "https://dtdreengus.firebaseio.com",
+      projectId: "dtdnavigator",
+      storageBucket: "dtdnavigator.appspot.com",
+      messagingSenderId: "381118272786",
+      //appId: "1:381118272786:web:7721ceb096f806bcec0fcb"
+    };
+    databaseName = "dtdreengus";
+    databaseURL = "https://dtdreengus.firebaseio.com";
+  }
+  else if (city == 'test') {
+    firebase = {
+      apiKey: "AIzaSyBGZ_IB4y5Ov1nuqIhWndGU8hfJadlE85I",
+      authDomain: "dtdnavigator.firebaseapp.com",
+      databaseURL: "https://dtdnavigatortesting.firebaseio.com",
+      projectId: "dtdnavigator",
+      storageBucket: "dtdnavigator.appspot.com",
+      messagingSenderId: "381118272786"
+    };
+    databaseName = "dtdnavigatortesting";
+    databaseURL = "https://dtdnavigatortesting.firebaseio.com";
+  }
+  else if (city == 'demo') {
+    firebase = {
+      apiKey: "AIzaSyBGZ_IB4y5Ov1nuqIhWndGU8hfJadlE85I",
+      authDomain: "dtdnavigator.firebaseapp.com",
+      databaseURL: "https://dtdjaipur.firebaseio.com",
+      projectId: "dtdnavigator",
+      storageBucket: "dtdnavigator.appspot.com",
+      messagingSenderId: "381118272786",
+    };
+    databaseName = "dtdjaipur";
+    databaseURL = "https://dtdjaipur.firebaseio.com";
+  }
+}
 
 @NgModule({
   imports: [
