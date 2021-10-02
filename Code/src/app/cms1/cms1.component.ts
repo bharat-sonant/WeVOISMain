665c1d3f8871d53774bd37a26150d2fe741c3f7c
hsc-soft <hsc.wevois@gmail.com>
2021-10-02 11:33:25 +0530

    cms1 committed

--- Code/src/app/cms1/cms1.component.ts
+++ Code/src/app/cms1/cms1.component.ts
@@ -18,46 +18,128 @@ export class Cms1Component implements OnInit {
   }
 
   setData() {
-    let dbPath = "WastebinMonitor/ImagesData/2021-10-02";
-    console.log(dbPath);
+    let date = "2021-10-02";
+    let dbPath = "WastebinMonitor/ImagesData/" + date;
     let dataInstance = this.db.object(dbPath).valueChanges().subscribe(
       data => {
         dataInstance.unsubscribe();
         if (data != null) {
           let keyArray = Object.keys(data);
           if (keyArray.length > 0) {
-            let transferIndex=0;
-            let openIndex=0;
-            let litterIndex=0;
-            let roadIndex=0;
-            let addindex=0;
+            let transferIndex = 0;
+            let openIndex = 0;
+            let litterIndex = 0;
+            let roadIndex = 0;
+            let addindex = 0;
             for (let i = 0; i < keyArray.length; i++) {
               let index = keyArray[i];
               if (data[index]["category"] != null) {
                 let dataObject = data[index];
                 let category = data[index]["category"];
-                if(category=="1"){
-                  transferIndex=transferIndex+1;
-                  addindex=transferIndex;
-                } else if(category=="2"){
-                  openIndex=openIndex+1;
-                  addindex=openIndex;
-                } else if(category=="3"){
-                  litterIndex=litterIndex+1;
-                  addindex=litterIndex;
-                } else if(category=="4"){
-                  roadIndex=roadIndex+1;
-                  addindex=roadIndex;
+                if (category == "1") {
+                  transferIndex = transferIndex + 1;
+                  addindex = transferIndex;
+                } else if (category == "2") {
+                  openIndex = openIndex + 1;
+                  addindex = openIndex;
+                } else if (category == "3") {
+                  litterIndex = litterIndex + 1;
+                  addindex = litterIndex;
+                } else if (category == "4") {
+                  roadIndex = roadIndex + 1;
+                  addindex = roadIndex;
                 }
-                dbPath = "WastebinMonitor/ImagesData/2021/October/2021-10-02/" + category + "/" + addindex;
+                dbPath = "WastebinMonitor/ImagesData/2021/October/" + date + "/" + category + "/" + addindex;
                 this.db.object(dbPath).update(dataObject);
               }
             }
+
+            let preTotal = 0 + transferIndex;
+            dbPath = "WastebinMonitor/Summary/CategoryWise/1";
+            this.db.object(dbPath).update({ totalCount: preTotal });
+
+            preTotal = 251 + openIndex;
+            dbPath = "WastebinMonitor/Summary/CategoryWise/2";
+            this.db.object(dbPath).update({ totalCount: preTotal });
+
+            preTotal = 16 + litterIndex;
+            dbPath = "WastebinMonitor/Summary/CategoryWise/3";
+            this.db.object(dbPath).update({ totalCount: preTotal });
+
+            preTotal = 0 + roadIndex;
+            dbPath = "WastebinMonitor/Summary/CategoryWise/4";
+            this.db.object(dbPath).update({ totalCount: preTotal });
+
+            //  this.getCategoryWiseTotal(transferIndex, "1");
+            //   this.getCategoryWiseTotal(openIndex, "2");
+            //  this.getCategoryWiseTotal(litterIndex, "3");
+           //   this.getCategoryWiseTotal(roadIndex, "4");
+
+            this.getDateWiseTotal(transferIndex, "1", date);
+            this.getDateWiseTotal(openIndex, "2", date);
+            this.getDateWiseTotal(litterIndex, "3", date);
+            this.getDateWiseTotal(roadIndex, "4", date);
+
+
+            // let total = transferIndex + openIndex + litterIndex + roadIndex;
+
+            dbPath = "WastebinMonitor/Summary/CategoryWise/totalCount";
+            let totalInstance = this.db.object(dbPath).valueChanges().subscribe(
+              count => {
+                totalInstance.unsubscribe();
+                let total = 267+ transferIndex + openIndex + litterIndex + roadIndex;
+                // if (count != null) {
+                //    total = 267 + Number(total);
+                //  }
+                dbPath = "WastebinMonitor/Summary/CategoryWise";
+                this.db.object(dbPath).update({ totalCount: total });
+              }
+            );
+
+            dbPath = "WastebinMonitor/Summary/DateWise/" + date + "/totalCount";
+            let dateWiseInstance = this.db.object(dbPath).valueChanges().subscribe(
+              count => {
+                dateWiseInstance.unsubscribe();
+                let total1 = transferIndex + openIndex + litterIndex + roadIndex;
+                // if (count != null) {
+                //   total = 267 + Number(total);
+                // }
+                dbPath = "WastebinMonitor/Summary/DateWise/" + date;
+                this.db.object(dbPath).update({ totalCount: total1 });
+              }
+            );
           }
         }
       }
     );
-
   }
 
+  getCategoryWiseTotal(total: any, category: any) {
+    let dbPath = "WastebinMonitor/Summary/CategoryWise/" + category + "/totalCount";
+    let categoryInstance = this.db.object(dbPath).valueChanges().subscribe(
+      count => {
+        categoryInstance.unsubscribe();
+        if (count != null) {
+
+          total = Number(count) + Number(total);
+        }
+        dbPath = "WastebinMonitor/Summary/CategoryWise/" + category;
+        this.db.object(dbPath).update({ totalCount: total });
+      }
+    );
+  }
+
+  getDateWiseTotal(total: any, category: any, date: any) {
+    let dbPath = "WastebinMonitor/Summary/DateWise/" + date + "/" + category + "/totalCount";
+    let categoryInstance = this.db.object(dbPath).valueChanges().subscribe(
+      count => {
+        categoryInstance.unsubscribe();
+        if (count != null) {
+          total =  Number(total);
+        }
+        dbPath = "WastebinMonitor/Summary/DateWise/" + date + "/" + category;
+        this.db.object(dbPath).update({ totalCount: total });
+      }
+    );
+  }
 }
