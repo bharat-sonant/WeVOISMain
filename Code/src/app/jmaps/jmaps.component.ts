16f983d671665a88e89488f86dcefa82b7952cc5
hsc-soft <hsc.wevois@gmail.com>
2021-10-06 17:02:06 +0530

    jmaps committed

--- Code/src/app/jmaps/jmaps.component.ts
+++ Code/src/app/jmaps/jmaps.component.ts
@@ -42,7 +42,7 @@ export class JmapsComponent {
   zoneKML: any;
   strokeWeight = 7;
   vehicleList: any[];
-  circleList:any[];
+  circleList: any[];
   progressData: progressDetail = {
     totalWardLength: 0,
     wardLength: "0",
@@ -67,7 +67,6 @@ export class JmapsComponent {
     });
     $("#txtDate").val(this.selectedDate);
     this.setHeight();
-    //this.getZones();
     this.setMaps();
     this.lines = [];
     this.polylines = [];
@@ -76,7 +75,7 @@ export class JmapsComponent {
   }
 
   changeCircleSelection(filterVal: any) {
-    this.selectedZone=0;
+    this.selectedZone = 0;
     this.resetAll();
     this.zoneList = [];
     let circleDetail = this.circleList.find(item => item.circleName == filterVal);
@@ -213,24 +212,29 @@ export class JmapsComponent {
   }
 
   getAllLinesFromJson() {
+    //this.zoneKML = this.commonService.setKML(this.selectedZone, this.map);
     this.httpService.get("../../assets/jsons/JaipurGreater/" + this.selectedZone + ".json").subscribe(data => {
       if (data != null) {
         var keyArray = Object.keys(data);
         if (keyArray.length > 0) {
-          for (let i = 1; i < keyArray.length; i++) {
+          for (let i = 0; i < keyArray.length; i++) {
             this.wardLines = keyArray.length;
             let lineNo = keyArray[i];
             if (data[lineNo] != null) {
               var latLng = [];
-              for (let j = 0; j < data[lineNo]["points"].length; j++) {
-                latLng.push({ lat: data[lineNo]["points"][j][0], lng: data[lineNo]["points"][j][1] });
+              if (data[lineNo]["points"] != undefined) {
+                if (data[lineNo]["points"].length > 0) {
+                  for (let j = 0; j < data[lineNo]["points"].length; j++) {
+                    latLng.push({ lat: data[lineNo]["points"][j][0], lng: data[lineNo]["points"][j][1] });
+                  }
+                  this.lines.push({
+                    lineNo: lineNo,
+                    latlng: latLng,
+                    color: "#fa0505",
+                  });
+                  this.plotLineOnMap(lineNo, latLng, i - 1, this.selectedZone);
+                }
               }
-              this.lines.push({
-                lineNo: i,
-                latlng: latLng,
-                color: "#87CEFA",
-              });
-              this.plotLineOnMap(i, latLng, i - 1, this.selectedZone);
             }
           }
         }
@@ -253,7 +257,7 @@ export class JmapsComponent {
                 this.lines.push({
                   lineNo: i,
                   latlng: latLng,
-                  color: "#87CEFA",
+                  color: "#fa0505",
                 });
                 this.plotLineOnMap(i, latLng, i - 1, this.selectedZone);
               }
@@ -264,13 +268,106 @@ export class JmapsComponent {
     });
   }
 
+  resetAllLines() {
+    if (this.lines.length > 0) {
+      for (let j = 0; j < this.lines.length; j++) {
+        let line = new google.maps.Polyline(this.polylines[j]);
+        var polyOptions = {
+          strokeColor: "#fa0505",
+          strokeOpacity: 1.0,
+          strokeWeight: this.strokeWeight
+        }
+        line.setOptions(polyOptions);
+        let lineNo = this.lines[j]["lineNo"];
+        let dbPath = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/LineStatus/" + lineNo + "/Status";
+        let dbPath2 = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/LineStatus/" + lineNo + "/Time";
+        this.db.database.ref(dbPath).set(null);
+        this.db.database.ref(dbPath2).set(null);
+      }      
+      
+      let dbPath = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/Summary";
+      this.db.database.ref(dbPath).set(null);
+      this.progressData.coveredLength = "0";
+      this.progressData.workPercentage = 0 + "%";
+    }
+  }
+
+
+  selectAll() {
+    if (this.lines.length > 0) {
+      let dist = 0;
+      for (let j = 0; j < this.lines.length; j++) {
+        let line = new google.maps.Polyline(this.polylines[j]);
+        var polyOptions = {
+          strokeColor: "#28a745",
+          strokeOpacity: 1.0,
+          strokeWeight: this.strokeWeight
+        }
+        line.setOptions(polyOptions);
+        let latlngs = this.lines[j]["latlng"];
+
+        for (let i = latlngs.length - 1; i > 0; i--) {
+          let lat1 = latlngs[i]["lat"];
+          let lon1 = latlngs[i]["lng"];
+          let lat2 = latlngs[i - 1]["lat"];
+          let lon2 = latlngs[i - 1]["lng"];
+
+          const R = 6377830; // metres
+          const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+          const φ2 = lat2 * Math.PI / 180;
+          const Δφ = (lat2 - lat1) * Math.PI / 180;
+          const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+          const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+          const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+          dist = dist + (R * c);
+        }
+        let date = new Date();
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let second = date.getSeconds();
+        let time = (hour < 10 ? "0" : "") + hour + ":" + (min < 10 ? "0" : "") + min + ":" + (second < 10 ? "0" : "") + second;
+
+        let lineNo = this.lines[j]["lineNo"];
+        let dbPath = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/LineStatus/" + lineNo + "/Status";
+        let dbPath2 = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/LineStatus/" + lineNo + "/Time";
+        this.db.database.ref(dbPath).set("LineCompleted");
+        this.db.database.ref(dbPath2).set(time);
+
+      }
+
+      let wardCoveredDistance = dist;
+      let completedLines = this.lines.length - 1;
+      let workPercentage = 0;
+      workPercentage = Math.round((wardCoveredDistance * 100) / this.progressData.totalWardLength);
+
+      let userid = localStorage.getItem("userID");
+      const data1 = {
+        userid: userid,
+        completedLines: completedLines,
+        wardCoveredDistance: wardCoveredDistance.toFixed(0),
+        workPercentage: workPercentage
+      }
+      let dbPath = "WasteCollectionInfo/" + this.selectedZone + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/Summary";
+      this.db.object(dbPath).update(data1);
+      this.progressData.coveredLength = (parseFloat(wardCoveredDistance.toString()) / 1000).toFixed(2);
+      this.progressData.workPercentage = workPercentage + "%";
+    }
+  }
+
   plotLineOnMap(lineNo: any, latlngs: any, i: any, wardNo: any) {
     let dbPathLineStatus = "WasteCollectionInfo/" + wardNo + "/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate + "/LineStatus/" + lineNo + "/Status";
     let lineStatus = this.db.object(dbPathLineStatus).valueChanges().subscribe((status) => {
       lineStatus.unsubscribe();
+      let strockColor="#fa0505";
+      if(status=="LineCompleted"){
+        strockColor="#28a745";
+      }
       let line = new google.maps.Polyline({
         path: latlngs,
-        strokeColor: this.commonService.getLineColor(status),
+        strokeColor: strockColor,
         strokeWeight: this.strokeWeight,
       });
       this.polylines[i] = line;
@@ -315,7 +412,7 @@ export class JmapsComponent {
 
         // alert(dist);
 
-        let stockColor = "#60c2ff";
+        let stockColor = "#fa0505";
         let isNew = true;
 
         let statusInstance = dbEvent.object(dbPath).valueChanges().subscribe(
@@ -325,13 +422,13 @@ export class JmapsComponent {
               isNew = true;
               dbEvent.database.ref(dbPath).set("LineCompleted");
               dbEvent.database.ref(dbPath2).set(time);
-              stockColor = "#00f645";
+              stockColor = "#28a745";
             }
             else {
               isNew = false;
               dbEvent.database.ref(dbPath).set(null);
               dbEvent.database.ref(dbPath2).set(null);
-              stockColor = "#60c2ff";
+              stockColor = "#fa0505";
             }
             var polyOptions = {
               strokeColor: stockColor,
@@ -460,20 +557,13 @@ export class JmapsComponent {
     $("#divMap").css("height", $(window).height() - 80);
   }
 
-  getZones() {
-    this.zoneList=[];
-    this.commonService.getZones().then((zoneList:any) => {
-      this.zoneList=JSON.parse(zoneList);      
-    });
-  }
-
   getWardTotalLength() {
     this.commonService.getWardTotalLength(this.selectedZone).then((totalLength) => {
-      if(totalLength!=null){
+      if (totalLength != null) {
         this.progressData.wardLength = (parseFloat(totalLength.toString()) / 1000).toFixed(2);
         this.progressData.totalWardLength = Number(totalLength);
       }
-      else{
+      else {
         this.progressData.wardLength = "0.00";
         this.progressData.totalWardLength = 0;
       }
@@ -534,6 +624,7 @@ export class JmapsComponent {
     this.db.object(dbPath).update({ userid: localStorage.getItem("userID") });
     this.db.object(dbPath).update({ penalty: penalty });
   }
+
 }
 
 export class progressDetail {
