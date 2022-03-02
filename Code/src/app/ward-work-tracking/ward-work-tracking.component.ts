87ef4d128b26d322999d2f4dda571fc471d1863c
hsc-soft <hsc.wevois@gmail.com>
2022-03-02 09:35:47 +0530

    code committed for employee salary

---
+++ Code/src/app/ward-work-tracking/ward-work-tracking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ward-work-tracking',
+  templateUrl: './ward-work-tracking.component.html',
+  styleUrls: ['./ward-work-tracking.component.scss']
+})
+export class WardWorkTrackingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
