90c1928fd94139145e60fe42a0cab75a539a12e2
hsc-soft <hsc.wevois@gmail.com>
2022-03-02 11:10:38 +0530

    code committed for ward work tracking

--- Code/src/app/ward-work-tracking/ward-work-tracking.component.ts
+++ Code/src/app/ward-work-tracking/ward-work-tracking.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, ViewChild } from "@angular/core";
+//services
+import { CommonService } from "../services/common/common.service";
+import { FirebaseService } from "../firebase.service";
 
 @Component({
   selector: 'app-ward-work-tracking',
   templateUrl: './ward-work-tracking.component.html',
   styleUrls: ['./ward-work-tracking.component.scss']
 })
-export class WardWorkTrackingComponent implements OnInit {
+export class WardWorkTrackingComponent {
+  @ViewChild("gmap", null) gmap: any;
+  public map: google.maps.Map;
+  constructor(public fs: FirebaseService, private commonService: CommonService) { }
+  db: any;
+  public selectedZone: any;
+  zoneList: any[];
+  currentYear: any;
+  zoneKML: any;
+  selectedDate: any;
+  cityName: any;
+  progressData: progressDetail = {
 
-  constructor() { }
+  };
+
+  txtDate = "#txtDate";
 
   ngOnInit() {
+    this.cityName = localStorage.getItem("cityName");
+    this.db = this.fs.getDatabaseByCity(this.cityName);
+    this.setDefault();
   }
+
+  setDefault() {
+    this.selectedDate = this.commonService.setTodayDate();
+    $(this.txtDate).val(this.selectedDate);
+    this.setHeight();
+    this.setDefaultMap();
+    this.getZones().then(() => {
+      this.selectedZone = "0";
+    });
+  }
+
+  getWardData() {
+    this.setWardBoundary();
+  }
+
+  changeZoneSelection(filterVal: any) {
+    if (filterVal == "0") {
+      this.commonService.setAlertMessage("error", "Please select zone !!!");
+    }
+    this.selectedZone = filterVal;
+    this.getWardData();
+  }
+
+  setWardBoundary() {
+    this.commonService.setKML(this.selectedZone, this.zoneKML).then((data: any) => {
+      if (this.zoneKML != undefined) {
+        this.zoneKML[0]["line"].setMap(null);
+      }
+      this.zoneKML = data;
+      this.zoneKML[0]["line"].setMap(this.map);
+      const bounds = new google.maps.LatLngBounds();
+      for (let i = 0; i < this.zoneKML[0]["latLng"].length; i++) {
+        bounds.extend({ lat: Number(this.zoneKML[0]["latLng"][i]["lat"]), lng: Number(this.zoneKML[0]["latLng"][i]["lng"]) });
+      }
+      this.map.fitBounds(bounds);
+    });
+  }
+
+  setDefaultMap() {
+    var mapstyle = new google.maps.StyledMapType([
+      {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+    ]);
+    let mapProp = this.commonService.initMapProperties();
+    this.map = new google.maps.Map(this.gmap.nativeElement, mapProp);
+    this.map.mapTypes.set("styled_map", mapstyle);
+    this.map.setMapTypeId("styled_map");
+  }
+
+  getZones() {
+    return new Promise((resolve) => {
+      this.zoneList = [];
+      this.zoneList = JSON.parse(localStorage.getItem("latest-zones"));
+      this.zoneList[0]["zoneName"] = "--Select Zone--";
+      resolve(true);
+    });
+  }
+
+  setHeight() {
+    $(".navbar-toggler").show();
+    $("#divMap").css("height", $(window).height() - 80);
+  }
+}
+
+
+export class progressDetail {
 
 }
