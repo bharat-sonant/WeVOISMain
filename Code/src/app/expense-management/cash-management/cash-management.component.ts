af94201b00458cb9f7844ff86053b4cd089f4857
hsc-soft <hsc.wevois@gmail.com>
2022-03-22 08:53:41 +0530

    code committed for cash management design

--- Code/src/app/expense-management/cash-management/cash-management.component.ts
+++ Code/src/app/expense-management/cash-management/cash-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../services/common/common.service';
+import { FirebaseService } from "../../firebase.service";
 
 @Component({
   selector: 'app-cash-management',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashManagementComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public fs: FirebaseService, private commonService: CommonService) { }
+  toDayDate: any;
+  selectedMonth: any;
+  selectedYear: any;
+  yearList: any[] = [];
+  expenceList: any[] = [];
+  selectedMonthName: any;
+  db: any;
+  cityName: any;
   ngOnInit() {
+    this.cityName = localStorage.getItem("cityName");
+    this.commonService.chkUserPageAccess(window.location.href, this.cityName);
+    this.setDefault();
+  }
+
+  setDefault() {
+    this.db = this.fs.getDatabaseByCity(this.cityName);
+    this.toDayDate = this.commonService.setTodayDate();
+    this.selectedMonth = this.toDayDate.split('-')[1];
+    this.selectedYear = this.toDayDate.split('-')[0];
+    this.selectedMonthName = this.commonService.getCurrentMonthName(Number(this.selectedMonth) - 1);
+    this.getYear();
+    $('#ddlMonth').val(this.selectedMonth);
+    $('#ddlYear').val(this.selectedYear);
+  }
+
+  getYear() {
+    this.yearList = [];
+    let year = parseInt(this.toDayDate.split('-')[0]);
+    for (let i = year - 2; i <= year; i++) {
+      this.yearList.push({ year: i });
+    }
   }
 
 }
