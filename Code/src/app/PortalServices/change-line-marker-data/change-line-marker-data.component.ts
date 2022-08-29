4ceecda6987dde55e1d8722bb4507e114a9ca2a4
hsc-soft <hsc.wevois@gmail.com>
2022-08-29 15:11:00 +0530

    code committed for change line marker

--- Code/src/app/PortalServices/change-line-marker-data/change-line-marker-data.component.ts
+++ Code/src/app/PortalServices/change-line-marker-data/change-line-marker-data.component.ts
@@ -35,6 +35,7 @@ export class ChangeLineMarkerDataComponent implements OnInit {
   getZones() {
     this.zoneList = JSON.parse(localStorage.getItem("latest-zones"));
     this.zoneList.push({zoneNo:"132-R2",zoneName:"Zone 132-R2"});
+    this.zoneList.push({zoneNo:"134-R3",zoneName:"Zone 134-R3"});
     this.zoneList.push({zoneNo:"143-R3",zoneName:"Zone 143-R3"});
   }
 
