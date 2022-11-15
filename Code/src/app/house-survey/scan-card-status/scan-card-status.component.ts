4298a92d0b2d2d96021ff2ad417e7cbc4e88fbd5
hsc-soft <hsc.wevois@gmail.com>
2022-11-15 14:18:07 +0530

    code committed for scan card status

--- Code/src/app/house-survey/scan-card-status/scan-card-status.component.ts
+++ Code/src/app/house-survey/scan-card-status/scan-card-status.component.ts
@@ -15,11 +15,17 @@ export class ScanCardStatusComponent implements OnInit {
   cityName: any;
   scanCardList: any[] = [];
   scanCardFilterList: any[] = [];
-  progressList:any[]=[];
+  progressList: any[] = [];
   public lastUpdateDate: any;
   txtSearch = "#txtSearch";
   divLoader = "#divLoader";
-  rowsProgressListIndex:any;
+  replaceKey:any;
+  rowsProgressListIndex: any;
+  summaryData: summary = {
+    installed: 0,
+    unInstalled: 0,
+    date: "---"
+  };
 
   ngOnInit() {
     this.cityName = localStorage.getItem("cityName");
@@ -29,17 +35,53 @@ export class ScanCardStatusComponent implements OnInit {
 
   setDefault() {
     this.db = this.fs.getDatabaseByCity(this.cityName);
-    this.rowsProgressListIndex=30;
+    this.rowsProgressListIndex = 30;
+    this.setReplaceKey();
     this.getScanCardStatus();
+  }
+
+  setReplaceKey(){
+    const path="https://firebasestorage.googleapis.com/v0/b/dtdnavigator.appspot.com/o/CityDetails%2FCityDetails.json?alt=media";
+    let cityDataInstance=this.httpService.get(path).subscribe(cityData => {
+      cityDataInstance.unsubscribe();
+      if(cityData!=null){
+        let cityList=JSON.parse(JSON.stringify(cityData));
+        console.log(cityList);
+        console.log(this.commonService.getFireStoreCity());
+        let detail=cityList.find(item=>item.cityName==this.commonService.getFireStoreCity());
+        if(detail!=undefined)
+        {
+          this.replaceKey=detail.key;
+          console.log(this.replaceKey)
+        }
+      }
+    });
   }
 
   getScanCardStatus() {
     $(this.divLoader).show();
+    const lastUpdatePath="https://firebasestorage.googleapis.com/v0/b/dtdnavigator.appspot.com/o/" + this.commonService.getFireStoreCity() + "%2FCardScanData%2FlastUpdate.json?alt=media";
+    let lastUpdateInstance=this.httpService.get(lastUpdatePath).subscribe(lastUpdateData => {
+      lastUpdateInstance.unsubscribe();
+      if(lastUpdateData!=null){
+        this.summaryData.date = lastUpdateData["lastUpdate"];
+      }
+    });
     const path = "https://firebasestorage.googleapis.com/v0/b/dtdnavigator.appspot.com/o/" + this.commonService.getFireStoreCity() + "%2FCardScanData%2FscanCardStatus.json?alt=media";
     let scanCardStatusInstance = this.httpService.get(path).subscribe(scanCardStatusData => {
       scanCardStatusInstance.unsubscribe();
       if (scanCardStatusData != null) {
+
+        this.scanCardList=JSON.parse(JSON.stringify(scanCardStatusData));
+        this.scanCardList=this.commonService.transformNumeric(this.scanCardList,"serialNo");
+        this.summaryData.installed=this.scanCardList.filter(item=>item.cardInstalled=='yes').length;
+        this.summaryData.unInstalled=this.scanCardList.filter(item=>item.cardInstalled=='no').length;
+
+
+        /*
         this.lastUpdateDate = scanCardStatusData["lastUpdate"];
+        
+
         let keyArray = Object.keys(scanCardStatusData);
         if (keyArray.length > 0) {
           for (let i = 0; i < keyArray.length; i++) {
@@ -62,6 +104,8 @@ export class ScanCardStatusComponent implements OnInit {
             }
           }
         }
+
+        */
         this.scanCardFilterList = this.scanCardList;
         this.progressList = this.scanCardFilterList.slice(0, this.rowsProgressListIndex);
         $(this.divLoader).hide();
@@ -72,7 +116,7 @@ export class ScanCardStatusComponent implements OnInit {
   }
 
   getScanCardFilteredData() {
-    this.rowsProgressListIndex=30;
+    this.rowsProgressListIndex = 30;
     let serialNo = $(this.txtSearch).val();
     if (serialNo != "") {
       this.scanCardFilterList = this.scanCardList.filter(item => item.serialNo.toString().includes(serialNo));
@@ -83,7 +127,7 @@ export class ScanCardStatusComponent implements OnInit {
     this.progressList = this.scanCardFilterList.slice(0, this.rowsProgressListIndex);
   }
 
-  
+
   onContainerScroll() {
     let element = <HTMLElement>document.getElementById("divList");
     if ((element.offsetHeight + element.scrollTop + 30) >= element.scrollHeight) {
@@ -94,6 +138,7 @@ export class ScanCardStatusComponent implements OnInit {
 
   getScanCardData() {
     $(this.divLoader).show();
+    let cardList = [];
     let dbPath = "CardScanData";
     let scanCardDataInstance = this.db.object(dbPath).valueChanges().subscribe(
       scanCardData => {
@@ -105,18 +150,40 @@ export class ScanCardStatusComponent implements OnInit {
             for (let i = 0; i < keyArray.length; i++) {
               let key = keyArray[i];
               if (scanCardData[key]["serialNo"] != null) {
-                data[i.toString()] = { serialNo: scanCardData[key]["serialNo"], scanDate: scanCardData[key]["date"], cardInstalled: scanCardData[key]["cardInstalled"] };
+               // data[i.toString()] = { serialNo: scanCardData[key]["serialNo"], scanDate: scanCardData[key]["date"], cardInstalled: scanCardData[key]["cardInstalled"] };
+
+                let cardNo = scanCardData[key]["serialNo"];
+                let serialNo = Number(cardNo.toString().substring(this.replaceKey.length, cardNo.toString().length));
+                let date = "";
+                let scanDate = "";
+                let cardInstalled = "no";
+                if (scanCardData[key]["date"] != null) {
+                  date = scanCardData[key]["date"].split(' ')[0];
+                  scanDate = date.split('-')[2] + " " + this.commonService.getCurrentMonthShortName(Number(date.split('-')[1])) + " " + date.split('-')[0];
+                }
+                if (scanCardData[key]["cardInstalled"] != null) {
+                  cardInstalled = scanCardData[key]["cardInstalled"];
+                }
+                cardList.push({ cardNo: cardNo, scanDate: scanDate, cardInstalled: cardInstalled, serialNo: serialNo });
               }
             }
           }
           data["lastUpdate"] = this.commonService.setTodayDate();
           let filePath = "/CardScanData/";
-          this.commonService.saveJsonFile(data, "scanCardStatus.json", filePath);
+          this.commonService.saveJsonFile(data, "lastUpdate.json", filePath);
+          this.commonService.saveJsonFile(cardList, "scanCardStatus.json", filePath);
         }
         setTimeout(() => {
+          this.commonService.setAlertMessage("success","Data update successfully !!!");
           this.getScanCardStatus();
-        }, 6000);
+        }, 9000);
       })
   }
 
+}
+
+export class summary {
+  installed: number;
+  unInstalled: number;
+  date: string;
 }
