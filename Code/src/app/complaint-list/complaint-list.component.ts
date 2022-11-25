15d069e85dc1b1794410fc52952f5106e7fa4849
Abhay khandal <abhaykhandal2111@gmail.com>
2022-11-25 17:13:55 +0530

    code commited for complain list

--- Code/src/app/complaint-list/complaint-list.component.ts
+++ Code/src/app/complaint-list/complaint-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-complaint-list',
   templateUrl: './complaint-list.component.html',
   styleUrls: ['./complaint-list.component.scss']
 })
 export class ComplaintListComponent implements OnInit {
-
-  constructor() { }
+complainList:any[]=[];
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.getComplaint()
   }
+getComplaint(){
+  let path="https://firebasestorage.googleapis.com/v0/b/dtdnavigator.appspot.com/o/"+"Jaipur-Malviyanagar%2Fcity%2FComplaintrequest%2FComplaintrequest.json?alt=media"
+  let complainInstance=this.http.get(path).subscribe((complainJsondata)=>{
+    complainInstance.unsubscribe();
+    let keyArray=Object.keys(complainJsondata);
+    for(let i=0;i<keyArray.length;i++){
+      let key=keyArray[i];
+      if (key != "lastKey"){
+      let date=complainJsondata[key]["date"];
+      let complaintype=complainJsondata[key]["complaintype"];
+      let zone=complainJsondata[key]["zone"];
+      let name=complainJsondata[key]["name"];
+      let message=complainJsondata[key]["message"];     
+this.complainList.push({date:date,complaintype:complaintype,zone:zone,
+  name:name,message:message,key:key})
+    }}
+  })
+}}
 
-}
