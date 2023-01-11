c33fc10100ba6f608451eab90ca543ded20622b4
hsc-soft <hsc.wevois@gmail.com>
2023-01-11 14:07:04 +0530

    code committed

--- Code/src/app/portal-access/portal-access.component.ts
+++ Code/src/app/portal-access/portal-access.component.ts
@@ -70,6 +70,9 @@ export class PortalAccessComponent implements OnInit {
       } else if (this.accessCity[i]["city"] == "wevois-others") {
         $("#wevoisBox").show();
         isBaseCity = true;
+      } else if (this.accessCity[i]["city"] == "ratangarh") {
+        $("#ratangarhBox").show();
+        isBaseCity = true;
       } else if (this.accessCity[i]["city"] == "jaipur-jagatpura" || this.accessCity[i]["city"] == "jaipur-jhotwara" || this.accessCity[i]["city"] == "jaipur-malviyanagar" || this.accessCity[i]["city"] == "jaipur-mansarovar" || this.accessCity[i]["city"] == "jaipur-murlipura" || this.accessCity[i]["city"] == "jaipur-sanganer" || this.accessCity[i]["city"] == "jaipur-vidhyadhar") {
         $("#jaipurBox").show();
       }
