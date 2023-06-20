bf9697de5cf413b0c657aa7a7569b6435d6d2af6
hsc-soft <hsc.wevois@gmail.com>
2023-06-20 15:29:26 +0530

    code committed

--- Code/src/app/reports/payment-via-cheque-report/payment-via-cheque-report.component.ts
+++ Code/src/app/reports/payment-via-cheque-report/payment-via-cheque-report.component.ts
@@ -124,7 +124,11 @@ export class PaymentViaChequeReportComponent implements OnInit {
                 monthName = this.commonService.getCurrentMonthShortName(Number(month));
                 checkDateFormat = day + " " + monthName + " " + year;
                 let imageUrl = this.commonService.fireStoragePath + this.commonService.getFireStoreCity() + "%2FPaymentCollectionHistory%2FPaymentViaChequeImage%2F" + cardNo + "%2F" + dateData[key]["chequeDate"] + "%2F" + dateData[key]["image"] + "?alt=media";
-                chequeListJSON.push({ key: key, cardNo: cardNo, zone: dateData[key]["ward"], chequeNo: dateData[key]["chequeNo"], chequeDate: checkDateFormat, name: dateData[key]["name"], bankName: dateData[key]["bankName"], collectedBy: dateData[key]["collectedById"], collectedByName: dateData[key]["collectedByName"], collectedDate: collectedDate, collectionDate: collectionDate, amount: dateData[key]["amount"], monthYear: dateData[key]["monthYear"], transactionId: dateData[key]["transactionId"], transactionDate: dateData[key]["transactionDate"],transactionDateFormat:transactionDateFormat, timeStemp: timeStemp,imageUrl:imageUrl,status: dateData[key]["status"],declinedReason:dateData[key]["declinedReason"]});
+                let transactionId="NA";
+                if(dateData[key]["transactionId"]!=null){
+                  transactionId=dateData[key]["transactionId"];
+                }
+                chequeListJSON.push({ key: key, cardNo: cardNo, zone: dateData[key]["ward"], chequeNo: dateData[key]["chequeNo"], chequeDate: checkDateFormat, name: dateData[key]["name"], bankName: dateData[key]["bankName"], collectedBy: dateData[key]["collectedById"], collectedByName: dateData[key]["collectedByName"], collectedDate: collectedDate, collectionDate: collectionDate, amount: dateData[key]["amount"], monthYear: dateData[key]["monthYear"], transactionId: transactionId, transactionDate: dateData[key]["transactionDate"],transactionDateFormat:transactionDateFormat, timeStemp: timeStemp,imageUrl:imageUrl,status: dateData[key]["status"],declinedReason:dateData[key]["declinedReason"]});
               }
             }
           }
