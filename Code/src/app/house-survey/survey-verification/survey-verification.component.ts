701f22233d7620ebd7e9c6f2cfcded7c4a589ef5
hsc-soft <hsc.wevois@gmail.com>
2023-08-07 14:23:13 +0530

    code committed for remove delete functionality from survey verification

--- Code/src/app/house-survey/survey-verification/survey-verification.component.ts
+++ Code/src/app/house-survey/survey-verification/survey-verification.component.ts
@@ -468,46 +468,6 @@ export class SurveyVerificationComponent {
     this.modalService.dismissAll();
   }
 
-  openDeleteConfirmation(index: any) {
-    $("#hddIndexDelete").val(index);
-    $("#divMarkerDelete").show();
-  }
-
-  cancelNewMarkerDelete() {
-    $("#hddIndexDelete").val("0");
-    $("#divMarkerDelete").hide();
-  }
-
-  deleteNewMarkers() {
-    if (this.houseMarkerCardList.length > 0) {
-      let index = Number($("#hddIndexDelete").val().toString());
-      let markerNo = this.houseMarkerCardList[index]["markerNo"];
-      let lineNo = this.houseMarkerCardList[index]["lineNo"];
-      let dbPath = "SurveyVerifierData/MarkedHousesByVerifier/" + this.selectedZone + "/" + lineNo + "/" + markerNo;
-      this.db.object(dbPath).remove();
-      let list = [];
-      for (let i = 0; i < this.houseMarkerCardList.length; i++) {
-        if (i != index) {
-          list.push({ markerNo: this.houseMarkerCardList[i]["markerNo"], lineNo: this.houseMarkerCardList[i]["lineNo"], imageUrl: this.houseMarkerCardList[i]["imageUrl"], entityType: this.houseMarkerCardList[i]["entityType"], latLng: this.houseMarkerCardList[i]["latLng"], date: this.houseMarkerCardList[i]["date"], timeStemp: this.houseMarkerCardList[i]["timeStemp"] });
-        }
-      }
-      this.houseMarkerCardList = list;
-      if (this.cardMarkers.length > 0) {
-        for (let i = 0; i < this.cardMarkers.length; i++) {
-          this.cardMarkers[i]["marker"].setMap(null);
-        }
-      }
-      this.cardMarkers = [];
-      for (let i = 0; i < this.houseMarkerCardList.length; i++) {
-        let lat = this.houseMarkerCardList[i]["latLng"].split(",")[0];
-        let lng = this.houseMarkerCardList[i]["latLng"].split(",")[1];
-        let markerURL = this.getMarkerIcon("red");
-        this.setMarkers(lat, lng, markerURL, "");
-      }
-      this.cancelNewMarkerDelete();
-
-    }
-  }
 
   getMarkerIcon(color: any) {
     let markerIcon = "../assets/img/red-home.png";
