14d7553019f3d92463459a57f7107ff34f09f05e
hsc-soft <hsc.wevois@gmail.com>
2023-08-29 12:15:33 +0530

    code committed for pali city

--- Code/src/app/portal-access/portal-access.component.ts
+++ Code/src/app/portal-access/portal-access.component.ts
@@ -97,6 +97,9 @@ export class PortalAccessComponent implements OnInit {
       } else if (this.accessCity[i]["city"] == "dehradun") {
         $("#dehradunBox").show();
         isBaseCity = true;
+      } else if (this.accessCity[i]["city"] == "pali") {
+        $("#paliBox").show();
+        isBaseCity = true;
       } else if (this.accessCity[i]["city"] == "jaipur-jagatpura" || this.accessCity[i]["city"] == "jaipur-jhotwara" || this.accessCity[i]["city"] == "jaipur-malviyanagar" || this.accessCity[i]["city"] == "jaipur-mansarovar" || this.accessCity[i]["city"] == "jaipur-murlipura" || this.accessCity[i]["city"] == "jaipur-sanganer" || this.accessCity[i]["city"] == "jaipur-vidhyadhar") {
         $("#jaipurBox").show();
       }
