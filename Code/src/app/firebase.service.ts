7ed063c77ed61fdf3c8e029dee7d774365cd9526
hsc-soft <hsc.wevois@gmail.com>
2023-08-29 12:16:06 +0530

    code committed for pali city

--- Code/src/app/firebase.service.ts
+++ Code/src/app/firebase.service.ts
@@ -454,6 +454,18 @@ export class FirebaseService {
       databaseName = "dtddehradun";
       databaseURL = "https://dtddehradun.firebaseio.com";
     }
+    else if (city == 'pali') {
+      this.fireBase = {
+        apiKey: "AIzaSyBGZ_IB4y5Ov1nuqIhWndGU8hfJadlE85I",
+        authDomain: "dtdnavigator.firebaseapp.com",
+        databaseURL: "https://dtdpali.firebaseio.com",
+        projectId: "dtdnavigator",
+        storageBucket: "dtdnavigator.appspot.com",
+        messagingSenderId: "381118272786",
+      };
+      databaseName = "dtddehradun";
+      databaseURL = "https://dtdpali.firebaseio.com";
+    }
 
     return new AngularFireDatabase(
       _firebaseAppFactory(this.fireBase, databaseName),
