fac65730da43c7a951915e32c8b44c054dadafea
hsc-soft <hsc.wevois@gmail.com>
2023-09-26 11:19:09 +0530

    code committed

--- Code/src/app/home/home.component.ts
+++ Code/src/app/home/home.component.ts
@@ -74,7 +74,7 @@ export class HomeComponent implements OnInit {
             else {
               url = "https://main-wevois.firebaseapp.com/" + this.cityName + "/" + this.userid + "/" + newUrl;
             }
-            if (userAccessList[i]["url"].includes("deharadun-pmc")) {
+            if (userAccessList[i]["url"].includes("dehradun-pmc")) {
               if (this.isDehradun == true) {
                 this.accessList.push({ name: userAccessList[i]["name"], url: url, isShow: this.isShow, position: userAccessList[i]["position"], img: userAccessList[i]["img"], dataClass: dataClass, isOuterUrl: isOuterUrl });
               }
