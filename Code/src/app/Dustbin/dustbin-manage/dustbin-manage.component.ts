2d8879c83e06df9ff33f1c6163f80703bc8a1b2a
hsc-soft <hsc.wevois@gmail.com>
2023-11-06 08:31:03 +0530

    code committed for manage dustbin show disabled by name

--- Code/src/app/Dustbin/dustbin-manage/dustbin-manage.component.ts
+++ Code/src/app/Dustbin/dustbin-manage/dustbin-manage.component.ts
@@ -26,6 +26,7 @@ export class DustbinManageComponent implements OnInit {
   txtLng = "#txtLng";
   txtFreq = "#txtFreq";
   ddlDustbinType = "#ddlDustbinType";
+  isShowDisabledBy:any;
 
   dustbinSummary: dustbinSummary = {
     totalDustbin: 0,
@@ -41,6 +42,7 @@ export class DustbinManageComponent implements OnInit {
 
   getZoneList() {
     this.zoneList = [];
+    this.isShowDisabledBy="0";
     this.dustbinService.getDustbinZone().then((zones: any) => {
       if (zones != null) {
         let list = zones.toString().split(',');
@@ -75,17 +77,32 @@ export class DustbinManageComponent implements OnInit {
   getDustbins() {
     this.dustbinList = [];
     let list = [];
+    let userList=JSON.parse(localStorage.getItem("webPortalUserList"));
     if (this.dustbinStorageList.length > 0) {
       list = this.dustbinStorageList.filter(item => item.zone.toString().trim() == this.selectedZone.toString().trim());
       this.dustbinSummary.wardDustbin = list.filter(item => item.isDisabled != "yes").length;
       if (this.selectedStatus == "enabled") {
         list = list.filter(item => item.isDisabled != "yes");
+        this.isShowDisabledBy="0";
       }
       else if (this.selectedStatus == "disabled") {
         list = list.filter(item => item.isDisabled == "yes");
+        this.isShowDisabledBy="1";
+      }
+      else {
+        this.isShowDisabledBy="1";
+
       }
       for (let i = 0; i < list.length; i++) {
-        this.dustbinList.push({ zoneNo: list[i]["zone"], type: list[i]["type"], dustbin: list[i]["dustbin"], ward: list[i]["ward"], lat: list[i]["lat"], lng: list[i]["lng"], address: list[i]["address"], pickFrequency: list[i]["pickFrequency"], isDisabled: list[i]["isDisabled"] });
+        let disabledByName="";        
+        if(list[i]["disabledBy"]!=""){
+          let detail=userList.find(item=>item.userId==list[i]["disabledBy"]);
+          if(detail!=undefined){
+            disabledByName=detail.name;
+          }
+
+        }
+        this.dustbinList.push({ zoneNo: list[i]["zone"], type: list[i]["type"], dustbin: list[i]["dustbin"], ward: list[i]["ward"], lat: list[i]["lat"], lng: list[i]["lng"], address: list[i]["address"], pickFrequency: list[i]["pickFrequency"], isDisabled: list[i]["isDisabled"], disabledBy: list[i]["disabledBy"],disabledByName:disabledByName });
       }
     }
   }
@@ -199,6 +216,12 @@ export class DustbinManageComponent implements OnInit {
     let detail = this.dustbinStorageList.find(item => item.dustbin == dustbin);
     if (detail != undefined) {
       detail.isDisabled = status;
+      if (status == "yes") {
+        detail.disabledBy = localStorage.getItem("userID");
+      }
+      else {
+        detail.disabledBy = "";
+      }
     }
     localStorage.setItem("dustbin", JSON.stringify(this.dustbinStorageList));
     this.getDustbins();
