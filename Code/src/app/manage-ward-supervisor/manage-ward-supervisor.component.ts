1b18036a743966df990f23c01e424683a123a84c
hsc-soft <hsc.wevois@gmail.com>
2023-12-28 17:05:51 +0530

    code committed

--- Code/src/app/manage-ward-supervisor/manage-ward-supervisor.component.ts
+++ Code/src/app/manage-ward-supervisor/manage-ward-supervisor.component.ts
@@ -8,37 +8,40 @@ import { CommonService } from "../services/common/common.service";
   styleUrls: ['./manage-ward-supervisor.component.scss']
 })
 export class ManageWardSupervisorComponent implements OnInit {
-supervisorList:any[]=[];
-wardList:any[]=[];
-constructor(private http:HttpClient,public actRoute:ActivatedRoute,private commonService: CommonService) { }
-ngOnInit() {
+  supervisorList: any[] = [];
+  wardList: any[] = [];
+  constructor(private http: HttpClient, public actRoute: ActivatedRoute, private commonService: CommonService) { }
+  ngOnInit() {
+    this.commonService.savePageLoadHistory("Monitoring", "Manage-Ward-Supervisor", localStorage.getItem("userID"));
     this.wardList = JSON.parse(localStorage.getItem("markingWards"));
     this.getSupervisor()
   }
-getSupervisor(){
- const path=this.commonService.fireStoragePath+"VehicleMaintenence%2FMasters%2Fward-supervisor%2Fward-supervisor.json?alt=media"
-  let supervisorInstance=this.http.get(path).subscribe((supervisorJsondata)=>{
-  supervisorInstance.unsubscribe();
-  let keyArray = Object.keys(supervisorJsondata);
-  for (let i = 0; i < keyArray.length; i++) {
-    let key = keyArray[i];
-    if (key != "lastKey"){
-     
-      let supervisor=supervisorJsondata[key]["supervisor"];
-      this.supervisorList.push({supervisor:supervisor, key: key })
-    }
+  getSupervisor() {
+    const path = this.commonService.fireStoragePath + "VehicleMaintenence%2FMasters%2Fward-supervisor%2Fward-supervisor.json?alt=media"
+    let supervisorInstance = this.http.get(path).subscribe((supervisorJsondata) => {
+      supervisorInstance.unsubscribe();
+      let keyArray = Object.keys(supervisorJsondata);
+      for (let i = 0; i < keyArray.length; i++) {
+        let key = keyArray[i];
+        if (key != "lastKey") {
+
+          let supervisor = supervisorJsondata[key]["supervisor"];
+          this.supervisorList.push({ supervisor: supervisor, key: key })
+        }
+      }
+      this.supervisorList = [];
+      if (this.wardList.length > 0) {
+        for (let i = 0; i < this.wardList.length; i++) {
+          let wardNo = this.wardList[i]["zoneNo"];
+
+          this.supervisorList.push({ wardNo: wardNo });
+
+
+        }
+      }
+    })
   }
-  this.supervisorList=[];
-    if(this.wardList.length > 0) {
-    for (let i = 0; i < this.wardList.length; i++) {
-      let wardNo = this.wardList[i]["zoneNo"];
-    
-      this.supervisorList.push({ wardNo: wardNo});
 
 
-    }}})
-}
 
-
- 
 }
