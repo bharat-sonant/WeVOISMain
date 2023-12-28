6f312208bca23e05ab7e442d3f2af19a0864347d
hsc-soft <hsc.wevois@gmail.com>
2023-12-28 17:08:54 +0530

    code committed

--- Code/src/app/wardwise-scan-card/wardwise-scan-card.component.ts
+++ Code/src/app/wardwise-scan-card/wardwise-scan-card.component.ts
@@ -30,6 +30,8 @@ export class WardwiseScanCardComponent implements OnInit {
   ngOnInit() {
     this.cityName = localStorage.getItem("cityName");
     this.db = this.fs.getDatabaseByCity(this.cityName);
+    this.commonService.chkUserPageAccess(window.location.href, this.cityName);
+    this.commonService.savePageLoadHistory("Monitoring","Review-Scan-Card-Images",localStorage.getItem("userID"));
     this.setDefault();
   }
 
