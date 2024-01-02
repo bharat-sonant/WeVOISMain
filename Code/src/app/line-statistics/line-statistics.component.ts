dfb91e1bf9f02e15a420c5fb5626e08c8bbc666e
hsc-soft <hsc.wevois@gmail.com>
2024-01-02 17:09:59 +0530

    code committed

--- Code/src/app/line-statistics/line-statistics.component.ts
+++ Code/src/app/line-statistics/line-statistics.component.ts
@@ -93,6 +93,7 @@ export class LineStatisticsComponent implements OnInit {
     this.cityName = localStorage.getItem("cityName");
     this.db = this.fs.getDatabaseByCity(this.cityName);
     this.commonService.chkUserPageAccess(window.location.href, this.cityName);
+    this.commonService.savePageLoadHistory("Time-Distance","Time-Distance",localStorage.getItem("userID"));
     this.todayDate = this.commonService.setTodayDate();
     this.currentMonth = this.commonService.getCurrentMonthName(new Date(this.todayDate).getMonth());
     this.currentYear = new Date().getFullYear();
