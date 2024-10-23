5ce5f59fddfe97464c24a94595e280c9904703f2
hsc-soft <hsc.wevois@gmail.com>
2024-10-23 14:00:45 +0530

    code committed for work assign detail

--- Code/src/app/reports/work-assign-report/work-assign-report.component.ts
+++ Code/src/app/reports/work-assign-report/work-assign-report.component.ts
@@ -21,6 +21,9 @@ export class WorkAssignReportComponent implements OnInit {
   db: any;
   cityName: any;
   serviceName = "work-assign-report";
+  dutyOnByList: any[] = [];
+  dutyOffByList: any[] = [];
+  workFilterList: any[] = [];
   ngOnInit() {
     this.cityName = localStorage.getItem("cityName");
     this.db = this.fs.getDatabaseByCity(this.cityName);
@@ -30,55 +33,142 @@ export class WorkAssignReportComponent implements OnInit {
     $('#txtDate').val(this.selectedDate);
     this.currentMonthName = this.commonService.getCurrentMonthName(new Date(this.selectedDate).getMonth());
     this.currentYear = new Date().getFullYear();
-    this.getWorkAssigned();
+    this.getWorkAssignment();
   }
 
+  getWorkAssignment() {
+    let list = [];
+    let dbPath = "DailyWorkDetail/" + this.currentYear + "/" + this.currentMonthName + "/" + this.selectedDate;
+    let workInstance = this.db.object(dbPath).valueChanges().subscribe(data => {
+      workInstance.unsubscribe();
+      if (data != null) {
+        let keyArray = Object.keys(data);
+        for (let i = 0; i < keyArray.length; i++) {
+          let key = keyArray[i];
+          for (let i = 0; i <= 5; i++) {
+            if (data[key]["task" + i] != null) {
+              if (data[key]["task" + i]["task"] != null) {
+                let ward = data[key]["task" + i]["task"];
+                let startTime = "";
+                let endTime = "";
+                if (Object.keys(data[key]["task" + i + ""]["in-out"])[0] != null) {
+                  if (Object.values(data[key]["task" + i + ""]["in-out"])[0] == "In") {
+                    startTime = this.commonService.tConvert(Object.keys(data[key]["task" + i + ""]["in-out"])[0]);
+                    let removeSecond = startTime.split(" ");
+                    startTime = removeSecond[0].slice(0, -3) + " " + removeSecond[1];
+                  }
+                  else {
+                    endTime = this.commonService.tConvert(Object.keys(data[key]["task" + i + ""]["in-out"])[0]);
+                    let removeSecond = endTime.split(" ");
+                    endTime = removeSecond[0].slice(0, -3) + " " + removeSecond[1];
+                  }
 
-  getWorkAssigned() {
-    this.besuh.saveBackEndFunctionCallingHistory(this.serviceName, "getWorkAssigned");
-    this.workList = [];
-    let monthName = this.commonService.getCurrentMonthName(new Date(this.selectedDate).getMonth());
-    let year = this.selectedDate.split("-")[0];
-    let dbPath = "WhoAssignWork/" + year + "/" + monthName + "/" + this.selectedDate;
-    let workData = this.db.object(dbPath).valueChanges().subscribe(
-      Data => {
-        if (Data != null) {
-          this.besuh.saveBackEndFunctionDataUsesHistory(this.serviceName, "getWorkAssigned", Data);
-        }
-        workData.unsubscribe();
-        var keyArray = Object.keys(Data);
-        for (let index = 0; index < keyArray.length; index++) {
-          let empId = keyArray[index];
-          this.workList.push({ empId: empId, name: '', detail: '' });
-          let assignedList = Data[empId];
-          if (assignedList.length > 0) {
-            let detail = "";
-            for (let i = 0; i < assignedList.length; i++) {
-              if (assignedList[i] != null) {
-                if (detail == "") {
-                  detail = "<b>" + i + ".</b> Work assigned for " + assignedList[i]["task"] + " at " + assignedList[i]["time"];
                 }
-                else {
-                  detail = detail + "<br/><b>" + i + ".</b> Work assigned for " + assignedList[i]["task"] + " at " + assignedList[i]["time"];
+                if (Object.keys(data[key]["task" + i + ""]["in-out"])[1] != null) {
+                  if (Object.values(data[key]["task" + i + ""]["in-out"])[1] == "In") {
+                    startTime = this.commonService.tConvert(Object.keys(data[key]["task" + i + ""]["in-out"])[1]);
+                    let removeSecond = startTime.split(" ");
+                    startTime = removeSecond[0].slice(0, -3) + " " + removeSecond[1];
+                  }
+                  else {
+                    endTime = this.commonService.tConvert(Object.keys(data[key]["task" + i + ""]["in-out"])[1]);
+                    let removeSecond = endTime.split(" ");
+                    endTime = removeSecond[0].slice(0, -3) + " " + removeSecond[1];
+                  }
+                }
+                let dutyOffBy = "";
+                if (data[key]["task" + i]["task-dutyoff-by"] != null) {
+                  dutyOffBy = data[key]["task" + i]["task-dutyoff-by"];
+                }
+                let detail = list.find(item => item.ward == ward && item.inTime == startTime);
+                if (detail == undefined) {
+                  list.push({ ward: ward, inTime: startTime, outTime: endTime, dutyOnBy: data[key]["task" + i]["task-assigned-by"], dutyOffBy: dutyOffBy, dutyOnByName: "", dutyOffByName: "" });
                 }
               }
             }
-            this.workList[index]["detail"] = detail;
 
-            this.commonService.getEmplyeeDetailByEmployeeId(empId).then((employee) => {
-              let workDetails = this.workList.find(item => item.empId == empId);
-              if (workDetails != undefined) {
-                workDetails.name = employee["name"];
-              }
-            });
+          }
+
+        }
+        if (list.length > 0) {
+          this.workList = list.sort((a, b) =>
+            b.ward < a.ward ? 1 : -1
+          );
+          for (let i = 0; i < this.workList.length; i++) {
+            this.getEmployeeName(i, this.workList[i]["dutyOnBy"], "dutyOn");
+            if (this.workList[i]["dutyOffBy"] != "") {
+              this.getEmployeeName(i, this.workList[i]["dutyOffBy"], "dutyOff");
+            }
+            if (i == this.workList.length - 1) {
+              setTimeout(() => {
+                this.workFilterList = this.workList;
+              }, 200);
+            }
           }
         }
-      });
+        else {
+          this.commonService.setAlertMessage("error", "No record found !!!");
+        }
+      }
+      else {
+        this.commonService.setAlertMessage("error", "No record found !!!");
+      }
+    })
+
+  }
+
+  getFilteredData(empId: any, type: any) {
+    let list = this.workList;
+    if (type == "dutyOn") {
+      $("#drpDutyOff").val("0");
+      if (empId != "0") {
+        list = list.filter(item => item.dutyOnBy == empId);
+      }
+    }
+    else {
+      $("#drpDutyOn").val("0");
+      if (empId != "0") {
+        list = list.filter(item => item.dutyOffBy == empId);
+      }
+    }
+    this.workFilterList = list;
+  }
+
+  getEmployeeName(index: any, empId: any, type: any) {
+    this.commonService.getEmplyeeDetailByEmployeeId(empId).then((employee) => {
+      if (type == "dutyOn") {
+        this.workList[index]["dutyOnByName"] = employee["name"];
+        let detail = this.dutyOnByList.find(item => item.id == empId);
+        if (detail == undefined) {
+          this.dutyOnByList.push({ id: empId, name: employee["name"] });
+          this.dutyOnByList = this.dutyOnByList.sort((a, b) =>
+            b.name < a.name ? 1 : -1
+          );
+        }
+      } else {
+        this.workList[index]["dutyOffByName"] = employee["name"];
+        let detail = this.dutyOffByList.find(item => item.id == empId);
+        if (detail == undefined) {
+          this.dutyOffByList.push({ id: empId, name: employee["name"] });
+          this.dutyOffByList = this.dutyOffByList.sort((a, b) =>
+            b.name < a.name ? 1 : -1
+          );
+        }
+      }
+    });
+  }
+
+  resetData(){
+    $("#drpDutyOn").val("0");
+    $("#drpDutyOff").val("0");
+    this.workList = [];
+    this.workFilterList = [];
   }
 
   setDate(filterVal: any) {
     this.selectedDate = filterVal;
-    this.getWorkAssigned();
+    this.resetData();
+    this.getWorkAssignment();
 
   }
   setNextDate() {
@@ -86,14 +176,63 @@ export class WorkAssignReportComponent implements OnInit {
     let nextDate = this.commonService.getNextDate(currentDate, 1);
     $('#txtDate').val(nextDate);
     this.selectedDate = nextDate;
-    this.getWorkAssigned();
+    this.resetData();
+    this.getWorkAssignment();
   }
   setPreviousDate() {
     let currentDate = $('#txtDate').val();
     let previousDate = this.commonService.getPreviousDate(currentDate, 1);
     $('#txtDate').val(previousDate);
     this.selectedDate = previousDate;
-    this.getWorkAssigned();
+    this.resetData();
+    this.getWorkAssignment();
+  }
+
+  exportToExcel() {
+    if (this.workFilterList.length > 0) {
+      let htmlString = "";
+      htmlString = "<table>";
+      htmlString += "<tr>";
+      htmlString += "<td>";
+      htmlString += "Work";
+      htmlString += "</td>";
+      htmlString += "<td>";
+      htmlString += "Duty On Time";
+      htmlString += "</td>";
+      htmlString += "<td>";
+      htmlString += "Duty On By";
+      htmlString += "</td>";
+      htmlString += "<td>";
+      htmlString += "Duty Off Time";
+      htmlString += "</td>";
+      htmlString += "<td>";
+      htmlString += "Duty Off By";
+      htmlString += "</td>";
+      htmlString += "</tr>";
+
+      for (let i = 0; i < this.workFilterList.length; i++) {
+        htmlString += "<tr>";
+        htmlString += "<td t='s'>";
+        htmlString += this.workFilterList[i]["ward"];
+        htmlString += "</td>";
+        htmlString += "<td t='s'>";
+        htmlString += this.workFilterList[i]["inTime"];
+        htmlString += "</td>";
+        htmlString += "<td t='s'>";
+        htmlString += this.workFilterList[i]["dutyOnByName"];
+        htmlString += "</td>";
+        htmlString += "<td t='s'>";
+        htmlString += this.workFilterList[i]["outTime"];
+        htmlString += "</td>";
+        htmlString += "<td t='s'>";
+        htmlString += this.workFilterList[i]["dutyOffByName"];
+        htmlString += "</td>";
+        htmlString += "</tr>";
+      }
+      htmlString += "</table>";
+      let fileName = "Work-Assigned-Report - " + this.selectedDate + " .xlsx";
+      this.commonService.exportExcel(htmlString, fileName);
+    }
   }
 
 }
