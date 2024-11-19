3fc18705f90aca70edd5902cea7711f86519914f
hsc-soft <hsc.wevois@gmail.com>
2024-11-19 14:20:43 +0530

    code committed

--- Code/src/app/login/login.component.ts
+++ Code/src/app/login/login.component.ts
@@ -221,6 +221,12 @@ export class LoginComponent implements OnInit {
       else {
         localStorage.setItem("canViewAttendance", "0");
       }
+      if (userDetails.canUpdateDustbinPickDetail != null) {
+        localStorage.setItem("canUpdateDustbinPickDetail", userDetails.canUpdateDustbinPickDetail);
+      }
+      else {
+        localStorage.setItem("canUpdateDustbinPickDetail", "0");
+      }
       localStorage.setItem("notificationHalt", userDetails.notificationHalt);
       localStorage.setItem("haltDisableAccess", userDetails.haltDisableAccess);
       localStorage.setItem("notificationMobileDataOff", userDetails.notificationMobileDataOff);
